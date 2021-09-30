43533c854f827abbf6f7123e8b41d878c5f0f571
Jeffrey Tagen <jtagen@gmail.com>
2021-09-30 16:53:18 -0400

    Cleanup and directories

--- mamp_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<pre>";
-$connection = imap_open("{imap.gmail.com:993/imap/ssl/novalidate-cert}", "cse6214test@gmail.com", "msstatems") or die("can't connect: " . imap_last_error());
-$message_count = imap_check($connection);
-$range = "1:".$message_count->Nmsgs;
-$response = imap_fetch_overview($connection,$range);
-foreach ($response as $msg) 
-    print_r($msg);
-?>
