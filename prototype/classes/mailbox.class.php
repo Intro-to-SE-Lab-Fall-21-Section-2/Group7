abfb3a17f5d61d036ee887008ea5d01bc6806b25
ronunz <62888529+ronunz@users.noreply.github.com>
2021-10-18 15:47:27 -0500

    Documentation to Messages and Mailbox Class
    These changes are to reflect additional documentation made to the messages page and the components used in the mailbox class.

--- prototype/classes/mailbox.class.php
+++ prototype/classes/mailbox.class.php
@@ -4,27 +4,28 @@
 class Mailbox {
 	var $user;
 
-	function Mailbox(&$user) {
+	function Mailbox(&$user) { // passes variables to this class
 		$this->user = $user;
 	}
 
-	public static function TestConnection($server,$username,$password) {
-		$connection_string = "{" . $server . ":993/imap/ssl/novalidate-cert}";
-		$connection = @imap_open($connection_string, $username,$password);
+	public static function TestConnection($server,$username,$password) { // Tests imap connection
+		$connection_string = "{" . $server . ":993/imap/ssl/novalidate-cert}"; // constructs string to imap server
+		$connection = @imap_open($connection_string, $username,$password); // open IMAP stream, tests connection server, username, and password, returns IMAP instance
 		return $connection != false;
 	}
 
-	public function GetMessages($offset = 0) {
+	public function GetMessages($offset = 0) { // Get messages off IMAP server
 
-		$connection_string = "{" . $this->user->server . ":993/imap/ssl/novalidate-cert}";
-		$connection = @imap_open($connection_string, $this->user->username,$this->user->password);
+		$connection_string = "{" . $this->user->server . ":993/imap/ssl/novalidate-cert}"; // construct string to imap server
+		$connection = @imap_open($connection_string, $this->user->username,$this->user->password); // imap_open to start IMAP stream
 
-		$message_count = imap_check($connection);
+		$message_count = imap_check($connection); // check IMAP instance, returns object with properties Date, Driver, Mailbox name, Nmsgs, Recent number of messages
 
 		//TODO: implement offset checking based on $message_count->Nmsgs;
 		
-		$range = "1:".$message_count->Nmsgs;
-		$response = imap_fetch_overview($connection,$range);
+		$range = "1:".$message_count->Nmsgs; // Appends range for number of messages in inbox
+		$response = imap_fetch_overview($connection,$range); // Returns an ARRAY of objects describing single message in range
+		                                                     // See https://www.php.net/manual/en/function.imap-fetch-overview.php
 		return $response;
 	}
 }
--- prototype/messages.php
+++ prototype/messages.php
@@ -1,11 +1,12 @@
 <?php
-include("global.php");
-include("header.php");
+include("global.php"); // call mailbox class, mailbox function, test connection function, GetMessages 
+include("header.php"); // call user class, IsLoggedIn, Login Function, Logout Function
 
-$mbox = new Mailbox($_SESSION["user"]);
-$messages = $mbox->GetMessages(0);
+$mbox = new Mailbox($_SESSION["user"]); // pass user variables to mailbox
+$messages = $mbox->GetMessages(0); // An array of objects describing message
 
 ?>
+	  <!-- Buttons on along top of page -->
       <nav class="flex flex-wrap items-center justify-between p-4">
         <div class="flex flex-shrink-0"><a class="inline-block py-3 px-6 mt-4 lg:mt-0 leading-none text-green-600 bg-grey-300 hover:bg-grey-400 font-semibold rounded shadow" href="#">Compose</a></div>
         <div class="block lg:hidden">
@@ -21,6 +22,7 @@ $messages = $mbox->GetMessages(0);
         </div>
       </nav>
     
+      <!-- Botton on left handside of inbox -->
       <div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
         <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0">
       <div>
@@ -48,6 +50,7 @@ $messages = $mbox->GetMessages(0);
       </div>
 -->
       
+      <!-- Section is for displaying current email inbox  -->
       <section class="py-8 px-4">
         <h2 class="text-3xl mb-2 font-semibold font-heading font-semibold">Inbox</h2>
         <table class="w-full table-auto">
@@ -57,7 +60,7 @@ $messages = $mbox->GetMessages(0);
           <tbody>
 
           	<?php
-          	foreach ($messages as $message) { ?>
+          	foreach ($messages as $message) { ?> <!-- Displays each individual item in object array messages -->
 
 	            <tr>
 	              <td class="border-t px-2 py-2"><?php echo $message->from;?></td>
@@ -72,14 +75,15 @@ $messages = $mbox->GetMessages(0);
 	              	<? } ?>
 	              </td>
 	              <td class="text-center border-t px-2 py-2"><?php echo date("m/d/Y g:i a", strtotime($message->date));?></td>
-	              <td class="text-center border-t px-2 py-2"><a class="text-indigo-600 hover:underline" href="#">open</a></td>
+	              <td class="text-center border-t px-2 py-2"><a class="text-indigo-600 hover:underline" href="#">open <?php echo $message->unread?></a></td>
 	            </tr>
 		        <? } ?>
 
           </tbody>
         </table>
       </section>
-      
+     
+      <!--  Page buttonson bottom of page if we ever decide we want to use them
       <div class="flex p-4">
         <ul class="flex mx-auto md:ml-auto md:mx-0 list-reset border border-grey-light rounded">
           <li><a class="block px-3 py-2 text-indigo-600 hover:text-white hover:bg-indigo-700 border-r border-grey-light" href="#">Previous</a></li>
@@ -88,7 +92,7 @@ $messages = $mbox->GetMessages(0);
           <li><a class="block px-3 py-2 text-indigo-600 hover:text-white hover:bg-indigo-700 border-r border-grey-light" href="#">3</a></li>
           <li><a class="block px-3 py-2 text-indigo-600 hover:text-white hover:bg-indigo-700" href="#">Next</a></li>
         </ul>
-      </div>
+      </div> -->
       </div>
       </div>
 
