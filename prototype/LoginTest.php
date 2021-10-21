1bd7949240d5c37891de9303491c09e8e370f338
ronunz <62888529+ronunz@users.noreply.github.com>
2021-10-21 15:41:46 -0500

    Login Test
    Addition of Login unit test.

---
+++ prototype/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+include("classes/mailbox.class.php");
+include("classes/user.class.php");
+
+use PHPUnit\Framework\TestCase;
+
+session_start(); 
+
+class LoginTest extends TestCase {
+
+    public function testLoginSuccess() {
+    	$user = new User();
+
+    	$server = "imap.gmail.com";
+    	$username = "cse6214test@gmail.com";
+    	$password = "msstatems";
+    	$firstname = "John";
+    	$lastname = "Doe";
+
+        $this->assertEquals(true,$user->Login($server, $username,$password,$firstname,$lastname));
+    }
+
+
+    public function testLoginFail() {
+    	$user = new User();
+
+    	
+        $this->assertEquals(false,$user->Login("","","","",""));
+    }
+
+}
