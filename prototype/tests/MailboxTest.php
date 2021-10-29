aed686f69567714951dc9b5fc9ac96c07179b860
ronunz <62888529+ronunz@users.noreply.github.com>
2021-10-29 16:48:27 -0500

    Addition of MailBox Class test
    Adds testTestConnectionSucess

--- prototype/tests/MailboxTest.php
+++ prototype/tests/MailboxTest.php
@@ -1,12 +1,23 @@
 <?php
 
+include("prototype/classes/mailbox.class.php");
+
 use PHPUnit\Framework\TestCase;
 
 class xyz extends TestCase
 {
-    public function testxyz()
+    public function testTestConnectionSuccess()
     {
-        $this->assertTrue(true);
+        
+        $server = "imap.gmail.com";
+        $username = "cse6214test@gmail.com";
+        $password = "msstatems";
+        
+        $connection = @imap_open($connection_string, $username,$password);
+        
+        $this->assertEquals(true,$user->TestConnection($server, $username,$password));
+        
+        #$this->assertTrue(true);
     }
     
     public function testGetMessages()
