a2566e07c169c5cefe4c1bcb8fba4c71fc0f8b07
ronunz <62888529+ronunz@users.noreply.github.com>
2021-10-29 13:06:46 -0500

    Update user.class.php
    The IsLoggedIn method isn't used, so it has been removed.

--- prototype/classes/user.class.php
+++ prototype/classes/user.class.php
@@ -9,10 +9,6 @@ class User {
 	var $server;
 	// $this is used a local pseduo-variable for objects, https://www.geeksforgeeks.org/this-keyword-in-php/
 
-	function IsLoggedIn(){
-		return ($this->email != "");
-	}
-
 	function Login($server,$username,$password,$firstname,$lastname) { // Login using successful username and password by checking imap connectivity
 
 	    if (Mailbox::TestConnection($server,$username,$password,$firstname, $lastname)) { // @imap_open connection using username, password, server
